95ec9264b6a7e2902319dee8843205589855834a
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-01-21 22:35:47 +0100

    plugin nested_form_for refactoring

--- app/models/kebab.rb
+++ app/models/kebab.rb
@@ -1,8 +1,8 @@
 class Kebab < ActiveRecord::Base
   belongs_to :promoter, :class_name => 'Person'
-  accepts_nested_attributes_for :promoter, :reject_if => lambda { |a| a[:person_id].blank? }, :allow_destroy => true
   has_many :transportations
-  accepts_nested_attributes_for :transportations, :reject_if => lambda { |a| a[:person_id].blank? }, :allow_destroy => true
   has_many :participations
+  accepts_nested_attributes_for :promoter, :reject_if => lambda { |a| a[:person_id].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :transportations, :reject_if => lambda { |a| a[:person_id].blank? }, :allow_destroy => true
   accepts_nested_attributes_for :participations, :reject_if => lambda { |a| a[:person_id].blank? }, :allow_destroy => true
 end
