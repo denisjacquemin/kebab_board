c9138bc1643203fdada5243952e6f16a67d4a80c
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-02-01 20:51:27 +0100

    messages configured

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ config.action_view.cache_template_loading            = true
 # config.action_controller.asset_host = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.raise_delivery_errors = true
+#config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
 # Enable threaded mode
 # config.threadsafe!
