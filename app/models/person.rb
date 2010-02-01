2a056a5e3cdb93e82196620be847ab84f8d4c8a4
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-02-01 18:57:21 +0100

    Mr Bureaux check added on delete person

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,11 +2,19 @@ class Person < ActiveRecord::Base
   has_many :kebabs
   has_many :transportations
   
-  validate :name_mr_bureaux_cannot_be_updated, :on => :update
+  validate_on_update :name_mr_bureaux_cannot_be_updated
+  
+  before_destroy :name_mr_bureaux_cannot_be_deleted
   
   private
   def name_mr_bureaux_cannot_be_updated
     errors.add(:name, 'Ce nom ne peut etre modifie') if 
       Person.find(id, :select => 'name').name =='Mr Bureaux'
+  end
+  
+  def name_mr_bureaux_cannot_be_deleted
+    if Person.find(id, :select => 'name').name =='Mr Bureaux'
+      false
+    end 
   end
 end
