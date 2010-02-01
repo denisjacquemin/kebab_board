2371814d396aa90b3e77b0750ac8923f1a63b111
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-02-01 21:11:43 +0100

    host fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options = { :host => 'heroku.com' }
+config.action_mailer.default_url_options = { :host => 'kebab.heroku.com' }
 
 # Enable threaded mode
 # config.threadsafe!
