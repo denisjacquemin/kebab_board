86aae81f5e2f5f1c08883d05f9062f1660225c39
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-02-01 07:26:48 +0100

    clearence styling

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "clearance", "formtastic"
+  config.gem "clearance"
+  config.gem "formtastic"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
