6990ea2178d81162c1781809c0f45e555ac58175
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-02-01 18:34:14 +0100

    Check Mr bureaux moved to Person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,12 @@
 class Person < ActiveRecord::Base
   has_many :kebabs
   has_many :transportations
+  
+  validate :name_mr_bureaux_cannot_be_updated, :on => :update
+  
+  private
+  def name_mr_bureaux_cannot_be_updated
+    errors.add(:name, 'Ce nom ne peut etre modifie') if 
+      Person.find(id, :select => 'name').name =='Mr Bureaux'
+  end
 end
