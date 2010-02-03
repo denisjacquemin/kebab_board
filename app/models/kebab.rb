0f0be0b6ab593a133cc04059f089377db83c6d9a
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-02-03 18:19:39 +0100

    fix comments bug, no more comment saved if empty

--- app/models/kebab.rb
+++ app/models/kebab.rb
@@ -6,5 +6,5 @@ class Kebab < ActiveRecord::Base
   accepts_nested_attributes_for :promoter, :reject_if => lambda { |a| a[:person_id].blank? }, :allow_destroy => true
   accepts_nested_attributes_for :transportations, :reject_if => lambda { |a| a[:person_id].blank? }, :allow_destroy => true
   accepts_nested_attributes_for :participations, :reject_if => lambda { |a| a[:person_id].blank? }, :allow_destroy => true
-  accepts_nested_attributes_for :comments
+  accepts_nested_attributes_for :comments, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
