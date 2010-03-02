eabfbc1b24b59609b834f2349f2e5c7a0c71d920
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-03-02 12:48:32 +0100

    nested_form javascript updated

--- public/javascripts/nested_form.js
+++ public/javascripts/nested_form.js
@@ -1,42 +1,35 @@
-Event.observe(document, 'dom:loaded', function() {
-    $$('form a.add_nested_fields').each(function(a){
-        a.observe('click', addNestedField);  
-    });
-    
-    $$('form a.remove_nested_fields').each(function(a){
-        a.observe('click', removeField);  
-    });
+document.observe('click', function(e,el) {
+  if (el = e.findElement('form a.add_nested_fields')) {
+    el.observe('click', addNestedField);
+  }
+  if (el = e.findElement('form a.remove_nested_fields')) {
+    el.observe('click', removeField);
+  } 
 });
 
 function addNestedField(ev) {
     var element = ev.element();
-    
+
     // Setup
     var assoc   = element.readAttribute('data-association');
     var content = $(assoc + '_fields_blueprint').innerHTML;
-    
+
     // Make the context correct by replacing new_<parents> with the generated ID
     // of each of the parent objects
     var input = element.up('.fields').next('input');
     var name = '';
     if (input != undefined) name = input.readAttribute('name');
     var context = name.gsub(new RegExp('[[a-z]+]$'), ''); 
-    
-    alert('first:input: ' + $(this).parents('.fields').children('input:first').attr('name'));
-    
-    alert('name: ' + $(this).parents('.fields').children('input:first').attr('name') || '');
-    
-    alert('context: ' + context);
-    
+
     // context will be something like this for a brand new form:
     // project[tasks_attributes][1255929127459][assignments_attributes][1255929128105]
     // or for an edit form:
     // project[tasks_attributes][0][assignments_attributes][1]
-    
+
     if(context) {
       var parent_names = context.match(/[a-z]+_attributes/g) || [];
       var parent_ids   = context.match(/[0-9]+/g);
-      
+
       for(i = 0; i < parent_names.length; i++) {
         if(parent_ids[i]) {
           content = content.replace(
@@ -46,13 +39,13 @@ function addNestedField(ev) {
         }
       }
     }
-    
+
    // Make a unique ID for the new child          
    var regexp  = new RegExp('new_' + assoc, 'g');
    var new_id  = new Date().getTime();
    content     = content.replace(regexp, new_id);
-   
-   element.up('.fields').insert(content);
+
+   element.up('.fields').insert({bottom: content});
    return false;
 }
 
