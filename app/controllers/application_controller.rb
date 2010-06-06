66bd60eb18256b86493e7f9328ab71f80d7b6d41
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-06-06 16:19:46 +0200

    comments style improved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,11 +14,12 @@ class ApplicationController < ActionController::Base
   private
   
   def mobile_device?
-    if session[:mobile_param]  
-      session[:mobile_param] == "1"  
-    else  
-      request.user_agent =~ /Mobile|webOS/  
-    end
+    true
+    #if session[:mobile_param]  
+    #  session[:mobile_param] == "1"  
+    #else  
+    #  request.user_agent =~ /Mobile|webOS/  
+    #end
   end
   helper_method :mobile_device?
   
